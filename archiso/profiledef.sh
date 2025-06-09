29da6948b34bc6a8b2fd1403144a098e81bf95ec
Volodia Kraplich <v1mkss.m+git@gmail.com>
2025-06-09 15:15:36 +0300

    Initial configuration
    - Changes ID to arch
    - Adds polkit rule to allow wheel group members to execute
      actions without password authentication
    - Removes unnecessary requirements and bindings from pacman-init.service
    - Updates file permissions for new directories

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -19,6 +19,9 @@ bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--lon
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/etc/gshadow"]="0:0:400"
+  ["/etc/polkit-1/rules.d"]="0:0:750"
+  ["/etc/sudoers.d"]="0:0:750"
+  ["/etc/sudoers.d/g_wheel"]="0:0:440"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
