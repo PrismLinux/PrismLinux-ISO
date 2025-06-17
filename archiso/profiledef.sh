0d86416873b432af50a9ac0f2e9b95e71933055d
Volodia Kraplich <v1mkss.m+git@gmail.com>
2025-06-17 10:30:49 +0300

    Fix: Remove SOURCE_DATE_EPOCH usage from profiledef.sh

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -2,19 +2,21 @@
 # shellcheck disable=SC2034
 
 iso_name="CrystalLinux"
-iso_label="CL_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_label="CL_$(date +%Y%m)"
 iso_publisher="CrystalNetwork Studio <@CrystalNetwork Studio>"
 iso_application="CrystalLinux Live/Rescue DVD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-## GRUB
+quiet="n"
+work_dir="work"
+out_dir="out"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
-bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/etc/gshadow"]="0:0:400"
