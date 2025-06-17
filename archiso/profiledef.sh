af1d04bc6b72e2b8547125977c63db29e11d848c
Volodia Kraplich <v1mkss.m+git@gmail.com>
2025-06-17 19:12:54 +0300

    refactor

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -20,13 +20,13 @@ airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' 
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/etc/gshadow"]="0:0:400"
-  ["/etc/polkit-1/rules.d"]="0:0:750"
-  ["/etc/sudoers.d"]="0:0:750"
-  ["/etc/sudoers.d/g_wheel"]="0:0:440"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
-  ["/root/.gnupg"]="0:0:700"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/usr/local/bin/welcome-center"]="0:0:755"
+  ["/etc/sudoers.d"]="0:0:750"
+  ["/etc/sudoers.d/g_wheel"]="0:0:440"
+
+  ["/usr/bin/choose-mirror"]="0:0:755"
+  ["/usr/bin/welcome-center"]="0:0:755"
+  ["/usr/bin/intel-legacy-gpu-installer"]="0:0:755"
+  ["/usr/bin/nvidia-modules-installer"]="0:0:755"
 )
