4d4fd8206e280e3558c5df73492ff6862407ce1c
Volodia Kraplich <v1mkss.m+git@gmail.com>
2025-06-19 19:46:34 +0300

    using package for fish

--- archiso/airootfs/etc/skel/.config/fish/functions/__fish_command_not_found_handler.fish
+++
@@ -1,8 +0,0 @@
-function __fish_command_not_found_handler --on-event fish_command_not_found
-  set_color blue
-  echo -n "❄  "
-  set_color red
-  echo -n "$argv[1]"
-  set_color normal
-  echo " not found"
-end
