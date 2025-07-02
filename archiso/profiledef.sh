83c87d037a5730c174316d34ff7223bddc057f2c
Volodia Kraplich <v1mkss.m+git@gmail.com>
2025-07-02 23:01:54 +0300

    Refactor bootloader and kernel
    - Use grub as UEFI bootloader - Add grub configuration file - Switch to
    linux-zen kernel and add kernel parameters - Removed systemd and
    fallback boot options

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -11,7 +11,7 @@ buildmodes=('iso')
 quiet="n"
 work_dir="work"
 out_dir="out"
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.grub.esp' 'uefi-x64.grub.eltorito')
 
 arch="x86_64"
 pacman_conf="pacman.conf"
@@ -24,9 +24,7 @@ file_permissions=(
   ["/root/.automated_script.sh"]="0:0:755"
   ["/etc/sudoers.d"]="0:0:750"
   ["/etc/sudoers.d/g_wheel"]="0:0:440"
-
   ["/usr/bin/choose-mirror"]="0:0:755"
-  ["/usr/bin/welcome-center"]="0:0:755"
   ["/usr/bin/intel-legacy-gpu-installer"]="0:0:755"
   ["/usr/bin/nvidia-modules-installer"]="0:0:755"
 )
