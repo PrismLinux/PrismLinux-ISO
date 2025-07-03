b046ccd8a3aa7d40f7fe7106f5546c95ef5a0cbd
Volodia Kraplich <v1mkss.m+git@gmail.com>
2025-07-03 15:09:32 +0300

    Add Alacritty config and remove choose-mirror script

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -24,7 +24,6 @@ file_permissions=(
   ["/root/.automated_script.sh"]="0:0:755"
   ["/etc/sudoers.d"]="0:0:750"
   ["/etc/sudoers.d/g_wheel"]="0:0:440"
-  ["/usr/bin/choose-mirror"]="0:0:755"
   ["/usr/bin/configurate-desktop"]="0:0:755"
   ["/usr/bin/intel-legacy-gpu-installer"]="0:0:755"
 )
