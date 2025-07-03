7d14b3c79f8211615af630f0eb3050a995bef1f6
Volodia Kraplich <v1mkss.m+git@gmail.com>
2025-07-03 11:34:28 +0300

    Clean up mkinitcpio preset and update packages
    - Remove comments from linux.preset for clarity - Replace welcome-center
    with about in package list - Add configurate-desktop to file permissions

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -25,6 +25,7 @@ file_permissions=(
   ["/etc/sudoers.d"]="0:0:750"
   ["/etc/sudoers.d/g_wheel"]="0:0:440"
   ["/usr/bin/choose-mirror"]="0:0:755"
+  ["/usr/bin/configurate-desktop"]="0:0:755"
   ["/usr/bin/intel-legacy-gpu-installer"]="0:0:755"
   ["/usr/bin/nvidia-modules-installer"]="0:0:755"
 )
