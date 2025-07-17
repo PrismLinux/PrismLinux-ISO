5279e64254b24ee6f4475b1617a4148b41fdf23e
Volodia Kraplich <v1mkss.m+git@gmail.com>
2025-07-17 22:25:02 +0300

    removed g_wheel

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -23,7 +23,6 @@ file_permissions=(
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/etc/sudoers.d"]="0:0:750"
-  ["/etc/sudoers.d/g_wheel"]="0:0:440"
   ["/usr/bin/configurate-desktop"]="0:0:755"
   ["/usr/bin/intel-legacy-gpu-installer"]="0:0:755"
 )
