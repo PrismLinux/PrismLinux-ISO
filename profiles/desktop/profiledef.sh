9945d884f017afa81dc53efbd6b61325c9a1c1f1
Volodia Kraplich <v1mkss.m+git@gmail.com>
2025-08-27 18:15:04 +0300

    added profiles

---
+++ profiles/desktop/profiledef.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="PrismLinux"
+iso_label="PL_$(date +%Y%m)"
+iso_publisher="CrystalNetwork Studio"
+iso_application="PrismLinux Live/Rescue DVD"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+install_dir="prismlinux"
+buildmodes=('iso')
+quiet="n"
+work_dir="work"
+out_dir="out"
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.grub.esp' 'uefi-x64.grub.eltorito')
+
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:400"
+  ["/usr/bin/configurate-desktop"]="0:0:755"
+  ["/usr/bin/intel-legacy-gpu-installer"]="0:0:755"
+)
